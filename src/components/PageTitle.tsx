07b0cd300a5740a11965ca8990fbba4d8e0fe463
Mike Lay <mike@mkly.io>
2023-07-18 19:08:22 -0700

    Fix title

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from "react";
 
 export default function PageTitle({ children }: PropsWithChildren) {
   return (
-    <h1 className="text-4xl py-8 px-4 text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+    <h1 className="text-xl py-8 px-4 text-tremor-default text-tremor-content dark:text-dark-tremor-content">
       {children}
     </h1>
   );
